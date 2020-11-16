67c238dc70ccce17ab5a6b72c0dcc6b31b5ab474
Mikko Dequina <dequina1547@gmail.com>
2020-11-16 19:23:53 +0800

    changes on axios baseUrl

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: " http://localhost:8001",
-  // baseURL: "https://tinder-backend0001.herokuapp.com",
 });
 
 export default instance;
